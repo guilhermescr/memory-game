26467778231841e66fe0459a1eda9a0c32646776
guilhermescr <guilhermechaves2005@hotmail.com>
2022-06-24 18:15:40 -0300

    improved shuffling system

--- assets/js/modules/shuffleThemes.mjs
+++ assets/js/modules/shuffleThemes.mjs
@@ -1,5 +1,16 @@
+let randomThemes = document.querySelectorAll('.random-theme');
+let isShuffling = false;
+
 function shuffleThemes() {
-  alert('hi');
+  if (isShuffling) return;
+  isShuffling = true;
+
+  randomThemes.forEach((theme) => {
+    theme.classList.add('shuffleAnimationStart');
+    setTimeout(() => {
+      theme.classList.remove('shuffleAnimationStart');
+    }, 6000);
+  });
 }
 
 export { shuffleThemes };
