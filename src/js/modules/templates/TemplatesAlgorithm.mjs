0fffb9bb4fce01c25d95f4be27256921529adcee
guilhermescr <guilhermechaves2005@hotmail.com>
2022-09-25 20:34:23 -0300

    added templates section with 5 examples of templates

--- src/js/modules/templates/TemplatesAlgorithm.mjs
+++ src/js/modules/templates/TemplatesAlgorithm.mjs
@@ -1,18 +1,43 @@
-import { hideElements } from "../../main.js";
-import TEMPLATES_DATA from "./TemplatesData.mjs";
+import { hideElements, revealElements } from '../../main.js';
+import TEMPLATES_DATA from './TemplatesData.mjs';
 
 const CURRENT_TEMPLATE_IMAGE = document.querySelector('#currentTemplateImage');
-const CHANGE_TEMPLATE_BUTTON = document.querySelector('#changeTemplateButton');
+const CHANGE_TEMPLATE_BUTTON = document.querySelector(
+  '#changeCurrentTemplateButton'
+);
 const DEFAULT_TEMPLATE_IMAGE = TEMPLATES_DATA.ForestTemplate.CurrentTemplate;
+
+const HOME_SETTINGS_RETURN_ICON = document.querySelector(
+  '.home_settings_container .return-icon'
+);
+const HOME_SETTINGS_TITLE = document.querySelector('#home_settings_title');
 
 function showCurrentTemplateImage() {
   CURRENT_TEMPLATE_IMAGE.src = DEFAULT_TEMPLATE_IMAGE;
 }
 
 function changeTemplate() {
-  alert('Pardon me, but this feature is not implemented yet!');
-  // hideElements([document.querySelector('.templates_container'), document.querySelector('.audio-container')]);
+  // alert('Pardon me, but this feature is not implemented yet!');
+  HOME_SETTINGS_TITLE.innerHTML = 'Templates';
+  hideElements([
+    document.querySelector('.templates_container'),
+    document.querySelector('.audio-container')
+  ]);
+  revealElements(document.querySelector('.templates'));
+  HOME_SETTINGS_RETURN_ICON.style.display = 'block';
 }
+
+function hideTemplates() {
+  HOME_SETTINGS_TITLE.innerHTML = 'Settings';
+  HOME_SETTINGS_RETURN_ICON.style.display = 'none';
+  hideElements(document.querySelector('.templates'));
+  revealElements([
+    document.querySelector('.templates_container'),
+    document.querySelector('.audio-container')
+  ]);
+}
+
+HOME_SETTINGS_RETURN_ICON.onclick = hideTemplates;
 
 CHANGE_TEMPLATE_BUTTON.onclick = changeTemplate;
 
