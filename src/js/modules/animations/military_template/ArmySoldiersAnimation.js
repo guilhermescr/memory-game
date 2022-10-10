4fa156ef613b3d7af05a34536494f8bfe06c5ea4
guilhermescr <guilhermechaves2005@hotmail.com>
2022-10-10 14:00:04 -0300

    added a login page, added form validation

--- src/js/modules/animations/military_template/ArmySoldiersAnimation.js
+++ src/js/modules/animations/military_template/ArmySoldiersAnimation.js
@@ -1,4 +1,5 @@
 let ARMY_SOLDIERS_ANIMATION_INTERVAL, LEFT_SOLDIER, RIGHT_SOLDIER, BULLET;
+let ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST = [];
 
 function startArmySoldiersAnimation() {
   document.querySelector('.template_animation').innerHTML = `
@@ -13,10 +14,11 @@ function startArmySoldiersAnimation() {
   RIGHT_SOLDIER = document.querySelector('#right_army_soldier');
   BULLET = document.querySelector('#bullet');
 
-  startArmySoldiersAnimationInterval();
+  setArmySoldiersActions();
+  ARMY_SOLDIERS_ANIMATION_INTERVAL = setInterval(setArmySoldiersActions, 6500);
 }
 
-function startArmySoldiersAnimationInterval() {
+function setArmySoldiersActions() {
   leftSoldierShoot();
   rightSoldierGoesUp();
   rightSoldierShootToTheBottom();
@@ -24,107 +26,125 @@ function startArmySoldiersAnimationInterval() {
   rightSoldierGoesDown();
   rightSoldierShootToTheTop();
   leftSoldierGoesDown();
-
-  ARMY_SOLDIERS_ANIMATION_INTERVAL = setInterval(() => {
-    leftSoldierShoot();
-    rightSoldierGoesUp();
-    rightSoldierShootToTheBottom();
-    leftSoldierGoesUp();
-    rightSoldierGoesDown();
-    rightSoldierShootToTheTop();
-    leftSoldierGoesDown();
-  }, 6500);
 }
 
 function resetArmySoldiersAnimation() {
   clearInterval(ARMY_SOLDIERS_ANIMATION_INTERVAL);
+
+  // clear time out
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.forEach(
+    ARMY_SOLDIERS_ANIMATION_TIMEOUT => {
+      clearTimeout(ARMY_SOLDIERS_ANIMATION_TIMEOUT);
+    }
+  );
+
   document.querySelector('.template_animation').innerHTML = '';
 }
 
 function leftSoldierShoot() {
   BULLET.style.display = 'block';
-  // 2 seconds
   BULLET.classList.add('HorizontalBulletAnimation');
 
-  // bullet end
-  setTimeout(() => {
-    BULLET.classList.remove('HorizontalBulletAnimation');
-    BULLET.style.display = 'none';
-  }, 1500);
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+    setTimeout(() => {
+      BULLET.classList.remove('HorizontalBulletAnimation');
+      BULLET.style.display = 'none';
+    }, 1500)
+  );
 }
 
 function rightSoldierGoesUp() {
-  // right soldier start
-  setTimeout(() => {
-    RIGHT_SOLDIER.classList.add('RightSoldierAnimation');
-    rightSoldierStands();
-  }, 600);
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+    setTimeout(() => {
+      RIGHT_SOLDIER.classList.add('RightSoldierAnimation');
+      rightSoldierStands();
+    }, 600)
+  );
 }
 
 function rightSoldierStands() {
-  setTimeout(() => {
-    RIGHT_SOLDIER.style.transform = 'rotateY(180deg) rotate(0deg)';
-    RIGHT_SOLDIER.style.top = '5%';
-    RIGHT_SOLDIER.classList.remove('RightSoldierAnimation');
-  }, 1000);
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+    setTimeout(() => {
+      RIGHT_SOLDIER.style.transform = 'rotateY(180deg) rotate(0deg)';
+      RIGHT_SOLDIER.style.top = '5%';
+      RIGHT_SOLDIER.classList.remove('RightSoldierAnimation');
+    }, 1000)
+  );
 }
 
 function rightSoldierShootToTheBottom() {
-  setTimeout(() => {
-    BULLET.style.display = 'block';
-    BULLET.classList.add('RightSoldierShootToTheBottomAnimation');
-  }, 1600);
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+    setTimeout(() => {
+      BULLET.style.display = 'block';
+      BULLET.classList.add('RightSoldierShootToTheBottomAnimation');
+    }, 1600)
+  );
 }
 
 function leftSoldierGoesUp() {
-  setTimeout(() => {
-    // left soldier start
-    LEFT_SOLDIER.classList.add('LeftSoldierAnimation');
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
     setTimeout(() => {
-      BULLET.classList.remove('RightSoldierShootToTheBottomAnimation');
-      BULLET.style.display = 'none';
-      BULLET.style.top = '10%';
+      LEFT_SOLDIER.classList.add('LeftSoldierAnimation');
+      ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+        setTimeout(() => {
+          BULLET.classList.remove('RightSoldierShootToTheBottomAnimation');
+          BULLET.style.display = 'none';
+          BULLET.style.top = '10%';
 
-      LEFT_SOLDIER.style.top = '5%';
-      LEFT_SOLDIER.classList.remove('LeftSoldierAnimation');
-      leftSoldierShoot();
-    }, 1000);
-  }, 2000);
+          LEFT_SOLDIER.style.top = '5%';
+          LEFT_SOLDIER.classList.remove('LeftSoldierAnimation');
+          leftSoldierShoot();
+        }, 1000)
+      );
+    }, 2000)
+  );
 }
 
 function rightSoldierGoesDown() {
-  setTimeout(() => {
-    RIGHT_SOLDIER.classList.add('RightSoldierGoesDown');
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
     setTimeout(() => {
-      RIGHT_SOLDIER.style.transform = 'rotateY(180deg) rotate(-15deg)';
-      RIGHT_SOLDIER.style.top = '30%';
-      RIGHT_SOLDIER.classList.remove('RightSoldierGoesDown');
-    }, 1000);
-  }, 3600);
+      RIGHT_SOLDIER.classList.add('RightSoldierGoesDown');
+      ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+        setTimeout(() => {
+          RIGHT_SOLDIER.style.transform = 'rotateY(180deg) rotate(-15deg)';
+          RIGHT_SOLDIER.style.top = '30%';
+          RIGHT_SOLDIER.classList.remove('RightSoldierGoesDown');
+        }, 1000)
+      );
+    }, 3600)
+  );
 }
 
 function rightSoldierShootToTheTop() {
-  setTimeout(() => {
-    BULLET.style.display = 'block';
-    BULLET.classList.add('RightSoldierShootToTheTopAnimation');
-
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
     setTimeout(() => {
-      BULLET.style.display = 'none';
-      BULLET.classList.remove('RightSoldierShootToTheTopAnimation');
-    }, 1500);
-  }, 4600);
+      BULLET.style.display = 'block';
+      BULLET.classList.add('RightSoldierShootToTheTopAnimation');
+
+      ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+        setTimeout(() => {
+          BULLET.style.display = 'none';
+          BULLET.classList.remove('RightSoldierShootToTheTopAnimation');
+        }, 1500)
+      );
+    }, 4600)
+  );
 }
 
 function leftSoldierGoesDown() {
-  setTimeout(() => {
-    LEFT_SOLDIER.classList.add('LeftSoldierGoesDown');
-
+  ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
     setTimeout(() => {
-      LEFT_SOLDIER.style.top = '30%';
-      LEFT_SOLDIER.classList.remove('LeftSoldierGoesDown');
-      BULLET.style.top = '36%';
-    }, 1000);
-  }, 5000);
+      LEFT_SOLDIER.classList.add('LeftSoldierGoesDown');
+
+      ARMY_SOLDIERS_ANIMATION_TIMEOUT_LIST.push(
+        setTimeout(() => {
+          LEFT_SOLDIER.style.top = '30%';
+          LEFT_SOLDIER.classList.remove('LeftSoldierGoesDown');
+          BULLET.style.top = '36%';
+        }, 1000)
+      );
+    }, 5000)
+  );
 }
 
 export { startArmySoldiersAnimation, resetArmySoldiersAnimation };
---
+++ src/js/modules/auth/LoginService.mjs
@@ -0,0 +1,153 @@
+import {
+  allowGameToStart,
+  handleKeydownEvent,
+  hideElements,
+  renderLoaderContainer,
+  revealElements,
+  timeoutItems
+} from '../../main.js';
+import { addAlertMessage, removeAlertMessage } from './ValidationMessages.mjs';
+
+const SIGN_UP_IN_CONTAINER = document.querySelector('.sign_up_in_container');
+const FORM = document.querySelector('#sign_up_in_form');
+const USERNAME_INPUT = document.querySelector('.usernameInput');
+const PASSWORD_INPUT = document.querySelector('.passwordInput');
+const TOGGLE_PASSWORD_ICON = document.querySelector('.togglePassword');
+const PASSWORD_INFO = document.querySelector('#passwordInfo');
+
+const SHOW_PASSWORD_EYE = `
+<img
+src="../src/assets/images/icons/eye-password-show.svg"
+alt="Show Password Eye Icon"
+/>`;
+const HIDE_PASSWORD_EYE = `
+<img
+  src="../src/assets/images/icons/eye-password-hide.svg"
+  alt="Hide Password Eye Icon"
+/>`;
+
+const SUBMIT_BUTTON = document.querySelector('.submitButton');
+const PLAY_ANONYMOUSLY_BUTTON = document.querySelector('#playAnonymously');
+const SUCCESS_MESSAGE_PARAGRAPH = document.querySelector('.successMessage');
+
+const REGEX = new RegExp(
+  '^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$'
+);
+
+let type = 'password';
+
+function handleUsernameChange() {
+  removeAlertMessage(SUCCESS_MESSAGE_PARAGRAPH, 'success');
+
+  if (USERNAME_INPUT.value.length === 0) {
+    hideElements(SUCCESS_MESSAGE_PARAGRAPH);
+    revealElements(document.querySelector('.errorMessage'));
+    return;
+  }
+
+  hideElements(document.querySelector('.errorMessage'));
+  revealElements(SUCCESS_MESSAGE_PARAGRAPH);
+  SUCCESS_MESSAGE_PARAGRAPH.innerHTML = 'Checking username...';
+
+  timeoutItems(() => {
+    SUCCESS_MESSAGE_PARAGRAPH.classList.add('success');
+    SUCCESS_MESSAGE_PARAGRAPH.innerHTML = 'This username is available.';
+  });
+}
+
+function handlePasswordChange() {
+  removeAlertMessage(PASSWORD_INFO, 'error');
+  if (PASSWORD_INPUT.value.length === 0) {
+    hideElements(TOGGLE_PASSWORD_ICON);
+    type === 'text' ? togglePasswordVisibility() : null;
+    return;
+  }
+  revealElements(TOGGLE_PASSWORD_ICON);
+
+  if (REGEX.test(PASSWORD_INPUT.value)) {
+    return;
+  }
+
+  if (!REGEX.test(PASSWORD_INPUT.value)) {
+    addAlertMessage(PASSWORD_INFO, 'error');
+    return;
+  }
+}
+
+function togglePasswordVisibility() {
+  PASSWORD_INPUT.focus();
+
+  if (PASSWORD_INPUT.getAttribute('type') === 'text') {
+    type = 'password';
+    TOGGLE_PASSWORD_ICON.innerHTML = SHOW_PASSWORD_EYE;
+  } else {
+    type = 'text';
+    TOGGLE_PASSWORD_ICON.innerHTML = HIDE_PASSWORD_EYE;
+  }
+  PASSWORD_INPUT.setAttribute('type', type);
+}
+
+function handleSubmit() {
+  if (USERNAME_INPUT.value.length === 0) {
+    revealElements(document.querySelector('.errorMessage'));
+  }
+  if (PASSWORD_INPUT.value.length === 0) {
+    addAlertMessage(PASSWORD_INFO, 'error');
+  }
+  if (
+    USERNAME_INPUT.value.length !== 0 &&
+    PASSWORD_INPUT.value.length !== 0
+  ) {
+    endLogin();
+  }
+}
+
+function endLogin() {
+  allowGameToStart();
+  renderLoaderContainer();
+  document.body.removeChild(SIGN_UP_IN_CONTAINER);
+
+  window.addEventListener('keydown', function (event) {
+    let { key } = event;
+    handleKeydownEvent(key);
+  });
+
+  revealElements(document.querySelector('.toggleFullscreenIcon_container'));
+}
+
+USERNAME_INPUT.addEventListener('input', handleUsernameChange);
+
+USERNAME_INPUT.addEventListener('focus', () => {
+  if (USERNAME_INPUT.value.length !== 0) {
+    revealElements(SUCCESS_MESSAGE_PARAGRAPH);
+    SUCCESS_MESSAGE_PARAGRAPH.innerHTML = 'Checking username...';
+
+    timeoutItems(() => {
+      SUCCESS_MESSAGE_PARAGRAPH.classList.add('success');
+      SUCCESS_MESSAGE_PARAGRAPH.innerHTML = 'This username is available.';
+    });
+  }
+});
+
+USERNAME_INPUT.addEventListener('focusout', () => {
+  removeAlertMessage(SUCCESS_MESSAGE_PARAGRAPH, 'success');
+
+  hideElements(document.querySelector('.errorMessage'));
+  hideElements(SUCCESS_MESSAGE_PARAGRAPH);
+});
+
+PASSWORD_INPUT.addEventListener('input', handlePasswordChange);
+
+TOGGLE_PASSWORD_ICON.addEventListener('click', togglePasswordVisibility);
+
+FORM.addEventListener('submit', event => {
+  event.preventDefault();
+});
+SUBMIT_BUTTON.addEventListener('click', event => {
+  event.preventDefault();
+  handleSubmit();
+});
+
+PLAY_ANONYMOUSLY_BUTTON.addEventListener('click', endLogin);
+
+export { endLogin };
